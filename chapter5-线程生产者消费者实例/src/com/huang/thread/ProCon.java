937c58e609913da4e3de1de80d1320859283b9b2
huangshizeng <shizeng101@163.com>
2019-04-29 10:12:10 +0800

    使用wait和notify实现生产者消费者实例

--- chapter5-线程生产者消费者实例/src/com/huang/thread/ProCon.java
+++ chapter5-线程生产者消费者实例/src/com/huang/thread/ProCon.java
@@ -1,36 +1,98 @@
 package com.huang.thread;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
+ * 使用wait和notify实现生产者消费者实例
  * @author hsz
  */
 
-class Info {
+class Box {
 
-    private String name;
+    private Queue<String> queue;
+    private int size;
 
-    public String getName() {
-        return name;
+    public Box(Queue<String> queue, int size) {
+        this.queue = queue;
+        this.size = size;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public synchronized void put(String apple) {
+        while (queue.size() == size) {
+            try {
+                super.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        queue.offer(apple);
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("往箱子中放入一个苹果，总共有" + queue.size() + "个苹果");
+        super.notify();
+    }
+
+    public synchronized String get() {
+        while (queue.size() == 0) {
+            try {
+                super.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        String apple = queue.poll();
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("    从箱子中拿走一个苹果，还剩" + queue.size() + "个苹果");
+        super.notify();
+        return apple;
     }
 }
 
 class Producer implements Runnable {
-    private Info info;
+    private Box box;
 
-    public Producer(Info info) {
-        this.info = info;
+    public Producer(Box box) {
+        this.box = box;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 20; i++) {
+            box.put("apple");
+        }
+    }
+}
 
+class Consumer implements Runnable {
+    private Box box;
+
+    public Consumer(Box box) {
+        this.box = box;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            String apple = box.get();
         }
     }
 }
 
 public class ProCon {
+
+    public static void main(String[] args) {
+        Box box = new Box(new LinkedList<String>(), 5);
+        Producer producer = new Producer(box);
+        Consumer consumer = new Consumer(box);
+        new Thread(producer).start();
+        new Thread(consumer).start();
+    }
 }
