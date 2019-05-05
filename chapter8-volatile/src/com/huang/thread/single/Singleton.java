05fe1926da20481c6b9d3f300bb296fadc001a7e
huangshizeng <shizeng101@163.com>
2019-05-05 16:56:38 +0800

    volatile

---
+++ chapter8-volatile/src/com/huang/thread/single/Singleton.java
@@ -0,0 +1,31 @@
+package com.huang.thread.single;
+
+/**
+ * 单例模式
+ *
+ * @author hsz
+ */
+
+
+public class Singleton {
+
+    //volatile禁止重排序
+    private static volatile Singleton singleton;
+
+    public static Singleton getSingleton() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getSingleton();
+        Singleton singleton2 = Singleton.getSingleton();
+        System.out.println(singleton1 == singleton2);
+    }
+}
