5ef3078101c3197130a46419e3324adde3974f4b
huangshizeng <shizeng101@163.com>
2019-05-06 16:20:42 +0800

    CountDownLatch

---
+++ chapter10-并发工具类/src/com/huang/thread/countdownlatch/CountDownLatchTest.java
@@ -0,0 +1,74 @@
+package com.huang.thread.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author hsz
+ */
+
+
+public class CountDownLatchTest {
+
+    /**
+     * 使用join让主线程等待子线程执行完
+     */
+    public static void joinTest() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程1执行完");
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程2执行完");
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("主线程执行完");
+    }
+
+    /**
+     * 使用CountDownLatch让主线程等待子线程执行完
+     *
+     * @throws InterruptedException
+     */
+    public static void countDownLatchTest() throws InterruptedException {
+        CountDownLatch c = new CountDownLatch(2);
+        Thread t1 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程1执行完");
+            c.countDown();
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程2执行完");
+            c.countDown();
+        });
+        t1.start();
+        t2.start();
+        c.await();
+        System.out.println("主线程执行完");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        //CountDownLatchTest.joinTest();
+        CountDownLatchTest.countDownLatchTest();
+    }
+}
