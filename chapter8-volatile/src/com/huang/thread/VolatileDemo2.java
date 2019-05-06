bc45171d55118f7233c00c64d8651ed74d35c23e
huangshizeng <hsz101@qq.com>
2019-05-06 19:43:30 +0800

    补充

---
+++ chapter8-volatile/src/com/huang/thread/VolatileDemo2.java
@@ -0,0 +1,26 @@
+package com.huang.thread;
+
+/**
+ * @author hsz
+ */
+
+public class VolatileDemo2 {
+
+    static volatile int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new Thread(() -> {
+                for (int k = 0; k < 10000; k++) {
+                    count++;
+                }
+            });
+            threads[i].start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println(count);
+    }
+}
